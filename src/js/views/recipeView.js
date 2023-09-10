de94be3337257e27a7b8c9431b4d9cec996b9026
khaing17 <aungthetkhaing017@gmail.com>
2023-09-10 07:13:21 +0630

    Removing Fraction pkg and add fracty pkg

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -2,8 +2,7 @@
 
 import View from './View.js';
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
-
+import fracty from 'fracty';
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
   _errorMsg = 'We could not find that recipe. Please try another one!';
@@ -138,7 +137,7 @@ class RecipeView extends View {
       <use href="${icons}#icon-check"></use>
     </svg>
     <div class="recipe__quantity">${
-      ing.quantity ? new Fraction(ing.quantity).toString() : ''
+      ing.quantity ? new fracty(ing.quantity).toString() : ''
     }</div>
     <div class="recipe__description">
       <span class="recipe__unit">${ing.unit}</span>
